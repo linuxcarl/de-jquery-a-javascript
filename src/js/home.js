ec26c3c50cfc850db75f5f720a3182430c975775
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-09 14:20:14 -0500

    se agrego funcionalidad de ajax con jquery y con vanilla fetch

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,5 @@
+/*
+
 console.log('hola mundo!');
 const noCambia = "Leonidas";
 
@@ -6,3 +8,62 @@ let cambia = "@LeonidasEsteban"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise(function(todoBien, todoMal) {
+  // llamar a un api
+  setTimeout(function() {
+    // luego de 3 segundos
+    todoBien('se acabó el tiempo');
+  }, 5000)
+})
+
+const getUser = new Promise(function(todoBien, todoMal) {
+  // llamar a un api
+  setTimeout(function() {
+    // luego de 3 segundos
+    todoBien('se acabó el tiempo 3');
+  }, 3000)
+})
+
+// getUser
+//   .then(function() {
+//     console.log('todo está bien en la vida')
+//   })
+//   .catch(function(message) {
+//     console.log(message)
+//   })
+
+Promise.race([
+  getUser,
+  getUserAll,
+])
+.then(function(message) {
+  console.log(message);
+})
+.catch(function(message) {
+  console.log(message)
+})
+
+
+
+$.ajax('https://randomuser.me/api/sdfdsfdsfs', {
+  method: 'GET',
+  success: function(data) {
+    console.log(data)
+  },
+  error: function(error) {
+    console.log(error)
+  }
+})
+
+fetch('https://randomuser.me/api/')
+  .then( res => {
+    return res.json();
+  })
+  .then( data => {
+    console.log('user', data.results[0].name.first);
+  })
+  .catch(e => console.log(e));
+  */
+
+  
